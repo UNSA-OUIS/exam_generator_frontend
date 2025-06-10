1db0896314ede6ab480fa66c5dfbb1469015be38
RS7 <rsiza@unsa.edu.pe>
2025-06-10 14:55:15 -0500

    project arquitecture

---
+++ src/presentation/pages/login.tsx
@@ -0,0 +1,13 @@
+import { DSText } from "../components";
+
+const PageLogin = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <DSText />
+            <p>Welcome to login page!</p>
+        </div>
+    )
+}
+
+export default PageLogin;
