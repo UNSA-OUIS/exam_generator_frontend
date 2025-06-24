4488e1562ce5fd22ccebeab7532989aef814cb29
Edgar Mendoza <edmendozator@gmai.com>
2025-06-24 17:29:48 -0500

    styles navbar

---
+++ src/presentation/views/About.tsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import Sidebar from "../layouts/Sidebar";
+import Navbar from "../layouts/Navbar";
+import "../styles/main.css";
+
+export default function About() {
+  return (
+    <div className="bgcolor">
+      <Navbar />
+      <Box height={70} />
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <h1>About</h1>
+        </Box>
+      </Box>
+    </div>
+  );
+}
