5710cee28b3d8851995407c76d7dce164b066ec9
Edgar Mendoza <edmendozator@gmai.com>
2025-06-25 17:54:29 -0500

    login logout with context user and pass test

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext"; // Asegúrate de que la ruta sea correcta
+
+import Login from "./Login";
 import Home from "./views/Home";
 import About from "./views/About";
 import Settings from "./views/Settings";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </AuthProvider>
   );
 }
 
--- src/presentation/Login.tsx
+++ src/presentation/Login.tsx
@@ -1,18 +1,42 @@
-import { Grid, Paper, Avatar, TextField, Button, Stack, Box } from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { useState } from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Stack,
+  Box,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { useAuth } from "./context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const paperStyle = { padding: 20, width: 300 };
-  const avatarStyle = { backgroundColor: '#1bbd7e' };
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const { login, error } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    const success = await login(user, pass);
+    if (success) {
+      navigate("/home");
+    }
+  };
 
   return (
     <Box
       sx={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        bgcolor: '#f5f5f5', // opcional: fondo claro
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "#f5f5f5", // opcional: fondo claro
       }}
     >
       <Paper elevation={10} style={paperStyle}>
@@ -20,7 +44,9 @@ const Login = () => {
           <Avatar style={avatarStyle}>
             <LockOutlinedIcon />
           </Avatar>
-          <h3 style={{ textAlign: 'center' }}>SISTEMA DE GENERACIÓN DE EXAMENES</h3>
+          <h3 style={{ textAlign: "center" }}>
+            SISTEMA DE GENERACIÓN DE EXAMENES
+          </h3>
         </Grid>
 
         <Stack spacing={2} mt={2}>
@@ -30,6 +56,8 @@ const Login = () => {
             placeholder="Ingrese email"
             fullWidth
             required
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
           />
           <TextField
             type="password"
@@ -38,8 +66,11 @@ const Login = () => {
             placeholder="Ingrese contraseña"
             fullWidth
             required
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
           />
-          <Button variant="contained" fullWidth>
+          {error && <Typography color="error">{error}</Typography>}
+          <Button variant="contained" fullWidth onClick={handleLogin}>
             Validar
           </Button>
         </Stack>
@@ -48,4 +79,4 @@ const Login = () => {
   );
 };
 
-export default Login
+export default Login;
